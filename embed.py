5d8505d4ae1d1dfd4ac208b9c18c17ae95796752
Roi Cohen <roi@secondnature.ai>
2022-08-20 13:40:02 +0300

    embedder

--- embed.py
+++ embed.py
@@ -1,1 +1,11 @@
-class TemplateGenerator:
+import pandas as pd
+from transformers import T5ForConditionalGeneration, BertModel
+
+class Embedder:
+    def __init__(self, t5_for_template_generation, encoder_name):
+        self.template_generator = T5ForConditionalGeneration.from_pretrained(t5_for_template_generation)
+        self.encoder = BertModel.from_pretrained(encoder_name)
+
+    def forward(self, table_name, data):
+
+
